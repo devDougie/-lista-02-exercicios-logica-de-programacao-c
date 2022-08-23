eb07b9f6f6bcbab0d952f8b0d3e22255ec708ab7
Douglas C. Venancio <douglas13cv@gmail.com>
2022-08-23 10:40:17 -0300

    README

---
+++ exercicio-02/exercicio-02.c
@@ -0,0 +1,10 @@
+
+#include <stdio.h>
+
+int main(){
+	int valor;
+	puts("Digite um valor para ser exibido: ");
+	scanf("%d",&valor);
+	printf("O valor digitado pelo usuario e %d.",valor);
+	return 0;
+}
